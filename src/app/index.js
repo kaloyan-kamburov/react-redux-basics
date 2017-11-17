92e25f5cff239d36e199a7dace8a00916279ef3c
Kamburov <Kaloyan.Kamburov@experian.com>
2017-11-17 18:00:08 +0200

    video 4

--- src/app/index.js
+++ src/app/index.js
@@ -32,20 +32,34 @@
 
 import { createStore } from "redux";
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    lastValues: []
+}
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues]
+            };
+            state.lastValues.push(action.payload);
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues]
+            };
             break;
     }
 
     return state;
 };
 
-const store = createStore(reducer, 1);
+const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log("store updated!", store.getState())
