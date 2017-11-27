a4330d033336686eb69ca0f512335fc90e929d2b
kkamburov <Kaloyan.Kamburov@experian.com>
2017-11-27 17:35:11 +0200

    asyn functions

--- src/app/reducers/userReducer.js
+++ src/app/reducers/userReducer.js
@@ -4,7 +4,7 @@ const userReducer = (state = {
 }, action) => {
     
     switch (action.type) {
-        case "SET_NAME":
+        case "SET_NAME_FULFILLED":
             state = {
                 ...state,
                 name: action.payload
